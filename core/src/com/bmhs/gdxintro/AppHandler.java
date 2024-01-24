83466c4d02ea9f6539120bcc4a984468f3bc5f3a
dtrainor <djtrainor@icloud.com>
2024-01-24 12:10:45 -0800

    commit pls

--- core/src/com/bmhs/gdxintro/AppHandler.java
+++ core/src/com/bmhs/gdxintro/AppHandler.java
@@ -19,6 +19,7 @@ public class AppHandler extends ApplicationAdapter {
 	int x, y;
 	int z;
 	int a;
+	int b;
 
 	int[][] Canvas = {{0, 0, 0, 0, 0, 4, 4, 4, 4, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
 					 {0, 0, 0, 0, 4, 4, 3, 3, 3, 4, 4, 4, 4, 4, 4, 0, 0, 0, 0, 0},
